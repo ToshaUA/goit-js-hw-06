25dd382885b157200f31d8b0d55fe97bfe90257f
Anton Taran <91492704+ToshaUA@users.noreply.github.com>
2022-09-14 22:07:42 +0300

    Task 2
    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,9 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const elements = ingredients.map(element => {
+  const newEL = document.createElement('li');
+  newEL.classList.add('item');
+  newEL.textContent = element;
+  return newEL;
+});
+const ingredientsEl = document.querySelector('#ingredients');
+ingredientsEl.append(...elements);
